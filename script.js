6148d8898c5e8cc44113c8448258728695159f0b
Ben McGrath <ben.mcgrathdev@gmail.com>
2026-05-23 20:54:56 +0100

    Added simple Neuron page.

---
+++ script.js
@@ -0,0 +1,34 @@
+function sigmoid(i) {
+    return 1 / (1 + Math.exp(-i));
+}
+
+function neuron(w, b, i) {
+    return sigmoid((w * i) + b);
+}
+
+function twoInputNeuron(w, b, i, i2) {
+    return sigmoid((w * i) + (w * i2) + b);
+}
+
+var sout = document.getElementById("singNeuronOutput")
+var rsnb = document.getElementById("RunSingNeuronButton")
+
+rsnb.onclick = function () {
+    const w = parseFloat(prompt("Input weight: "));
+    const b = parseFloat(prompt("Input bias: "));
+    const i = parseFloat(prompt("Input input: "));
+
+    sout.innerHTML = "Output: " + String(neuron(w, b, i));
+}
+
+var tout = document.getElementById("twoNeuronOutput")
+var tsnb = document.getElementById("RunTwoNeuronButton")
+
+tsnb.onclick = function () {
+    const w = parseFloat(prompt("Input weight: "));
+    const b = parseFloat(prompt("Input bias: "));
+    const i = parseFloat(prompt("Input input 1: "));
+    const i2 = parseFloat(prompt("Input input 2: "));
+
+    tout.innerHTML = "Output: " + String(twoInputNeuron(w, b, i, i2));
+}
