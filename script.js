ec4e481605cdd8a776fa5ed8ef35db2357958294
Ben McGrath <ben.mcgrathdev@gmail.com>
2026-06-04 17:24:53 +0100

    Updated Index.html, script.js and neruonWork.html

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ function twoInputNeuron(w, w2, b, i, i2) {
     return sigmoid((w * i) + (w2 * i2) + b);
 }
 
+function threeInputNeuron(w, w2, w3, b, i, i2, i3) {
+    return sigmoid((w * i) + (w2 * i2) + (w3 * i3) + b);
+}
+
 var sout = document.getElementById("singNeuronOutput")
 var rsnb = document.getElementById("RunSingNeuronButton")
 
@@ -32,4 +36,19 @@ tsnb.onclick = function () {
     const i2 = parseFloat(prompt("Input input 2: "));
 
     tout.textContent = "Output: " + String(twoInputNeuron(w, w2, b, i, i2));
+}
+
+var thout = document.getElementById("threeNeuronOutput")
+var thsnb = document.getElementById("RunThreeNeuronButton")
+
+thsnb.onclick = function () {
+    const w = parseFloat(prompt("Input weight 1: "));
+    const w2 = parseFloat(prompt("Input weight 2: "));
+    const w3 = parseFloat(prompt("Input weight 3: "));
+    const b = parseFloat(prompt("Input bias: "));
+    const i = parseFloat(prompt("Input input 1: "));
+    const i2 = parseFloat(prompt("Input input 2: "));
+    const i3 = parseFloat(prompt("Input input 3: "));
+
+    thout.textContent = "Output: " + String(twoInputNeuron(w, w2, w3, b, i, i2, i3));
 }
